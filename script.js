686f18771423330ca048866a746bfb7fbe622ed5
ayeshasiddiqua197635-eng <ayeshasiddiqua197635@gmail.com>
2025-12-26 23:51:11 +0500

    Create script.js for future feature development
    Initialize script.js for future features.

---
+++ script.js
@@ -0,0 +1,6 @@
+// This file is for future features
+// Like login, signup, payments, dashboard
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("GlobalEarn website is running");
+});
